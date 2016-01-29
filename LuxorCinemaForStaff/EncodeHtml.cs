82d6b84a8dd756d09959ee5f48ba79f4c82fe0fe
skylog <nikbrons@gmail.com>
2016-01-29 18:11:07 +0200

    Was added the classes

---
+++ LuxorCinemaForStaff/EncodeHtml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace SimpleTest
+{
+    
+    public class EncodeHtml
+    {
+        //static string localhtml = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "simple_test.html");
+        private static string webhtml = "http://www.luxorfilm.ru/cinema/rostov-na-donu";
+
+        
+
+        public EncodeHtml()
+        {
+            var web = new HtmlWeb
+            {
+                AutoDetectEncoding = false,
+                OverrideEncoding = Encoding.UTF8,
+            };
+            Form1.doc = web.Load(webhtml);
+        }
+
+        public EncodeHtml(string insertHtml)
+        {
+            var web = new HtmlWeb
+            {
+                AutoDetectEncoding = false,
+                OverrideEncoding = Encoding.UTF8,
+            };
+            Form1.doc = web.Load(insertHtml);
+        }
+
+
+
+    }
+}
---
+++ LuxorCinemaForStaff/СombineSessionsTime.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTest
+{
+    public class СombineSessionsTime
+    {
+        public string SessionEndTime(string s1, string s2)
+        {
+            var s1Time = Convert.ToDateTime(DateTime.Parse(s1));
+            DateTime dt1 = Convert.ToDateTime(s1Time);
+            var s2Replase = s2.Replace("ч.", ":").Replace("мин.", "").Replace(" ", "");
+
+            if (s2Replase.Count() <= 5)
+            {
+                String pattern = @"([:])";
+                String[] elements = System.Text.RegularExpressions.Regex.Split(s2Replase, pattern);
+                var valueHour = Convert.ToInt32(elements[0]);
+                var valueMinute = Convert.ToInt32(elements[2]);
+
+                DateTime addTime = dt1.AddHours(valueHour).AddMinutes(valueMinute);
+                TimeSpan justTime = new TimeSpan(addTime.Hour, addTime.Minute, 0);
+                var res = Convert.ToString(justTime);
+                return res;
+            }
+            return "Упс";
+
+        }
+
+
+    }
+}
